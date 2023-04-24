8f17dfcc4ad6c576aa6524691883cf15b8af8b50
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2023-04-23 21:39:01 -0700

    feat(progress): initial progress

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('hello world');
+const { Command } = require('commander');
+const figlet = require('figlet');
+const pkg = require('../package.json');
+
+const program = new Command();
+
+const name = pkg.name;
+program
+  .name(name)
+  .version('0.0.1')
+  .description('Substrate CLI')
+  .command('new') // new
+  .description('Create a new Substrate project')
+  .argument('<name>', 'Name of the project')
+  .action((projectName: string) => {
+    console.log(`Creating new project ${projectName}`);
+  })
+  .command('update') // update
+  .description('Rebase your project on the latest Substrate version')
+  .action(() => {
+    console.log(`Updating ${name}`);
+  });
+
+console.log(
+  figlet.textSync('Substrate', {
+    font: 'Script',
+  }),
+);
+program.parse();
